6f4fdc98c83ed423d5f38dd319dce97c1fed858e
Artem Rylov <tema2707@icloud.com>
2020-09-29 10:56:59 +0700

    fist commit

---
+++ where-is-bus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  where-is-bus
+//
+//  Created by art-off on 29.09.2020.
+//  Copyright © 2020 art-off. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
